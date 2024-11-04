800b99c0b7f4da90882596989cf7a355987cd7a5
alessiogiovagnini <alessio.giovagnini@usi.ch>
2024-11-04 12:15:37 +0100

    added example

--- scripts/test-parser.py
+++ scripts/test-parser.py
@@ -5,6 +5,23 @@ parser = tsl.get_parser(language="julia")
 
 
 if __name__ == '__main__':
+    julia_function: str = """
+    function Base.getproperty(::SupervisedScitype{input_scitype, target_scitype, prediction_type},
+                              field::Symbol) where {input_scitype, target_scitype, prediction_type}
+        if field === :input_scitype
+            return input_scitype
+        elseif field === :target_scitype
+            return target_scitype
+        elseif field === :prediction_type
+            return prediction_type
+        else
+            throw(ArgumentError("Unsupported property. "))
+        end
+    end
+    """
+
+    tree = parser.parse(julia_function.encode())
+    node = tree.root_node
     pass
 
 
