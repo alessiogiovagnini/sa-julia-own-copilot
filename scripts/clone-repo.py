83ca85c67fd99f583c262da9812c5407c043bc31
alessiogiovagnini <alessio.giovagnini@usi.ch>
2024-11-08 10:25:14 +0100

    progress, need to fix error

--- scripts/clone-repo.py
+++ scripts/clone-repo.py
@@ -1,29 +1,35 @@
+import os
+from pandas import read_csv, DataFrame
 import sh
 from pathlib import Path
 import shutil
 from concurrent.futures import ThreadPoolExecutor
 import threading
 import random
+from julia_function_extractor import start_extraction
 
 
-def multi_thread_analysis(all_urls: list[str], max_threads: int = 16) -> None:
+def multi_thread_analysis(repo_names: list[str], max_threads: int = 16) -> None:
     with ThreadPoolExecutor(max_workers=max_threads) as executor:
-        executor.map(start_analysis, all_urls)
+        executor.map(start_analysis_on_repo, repo_names)
     pass
 
 
-def start_analysis(repo_name: str):
+def start_analysis_on_repo(repo_name: str):
     full_path: str = f"https://github.com/{repo_name}"
     current_thread_id: int = threading.get_ident()
     random_num: int = random.randrange(0, 1000000, 1)
-    tmp_destination: Path = Path(f"tmp-{current_thread_id}-{random_num}")
+    tmp_repo_dir: Path = Path("./repos")
+    tmp_repo_dir.mkdir(exist_ok=True)
+    tmp_destination: Path = Path(tmp_repo_dir, f"tmp-{current_thread_id}-{random_num}")
 
-    analyze_repo(url=full_path, destination=tmp_destination)
+    analyze_repo(url=full_path, destination=tmp_destination, repo_name=repo_name)
 
 
-def analyze_repo(url: str, destination: Path) -> None:
+def analyze_repo(url: str, destination: Path, repo_name: str) -> None:
     """
     clone a repository in the destination folder and analyze its content
+    :param repo_name: name of the repository, like: author/repo-name
     :param url: the url of the repository, like https://github.com/author/repo-name
     :param destination: path where the repo is cloned, the folder target will be created if it not exist
         and it will be DELETED at the end with all the repository
@@ -31,17 +37,32 @@ def analyze_repo(url: str, destination: Path) -> None:
     destination.mkdir(exist_ok=True)
     sh.git.clone(url, destination)
 
-    # TODO get every Julia file inside this repo and for each parse all the functions and save in the csv
+    csv_output_dir: Path = Path("./csv_output")
+    csv_output_dir.mkdir(exist_ok=True)
+
+    random_num: int = random.randrange(0, 1000000, 1)
+    csv_output_file: Path = Path(csv_output_dir, f"{repo_name}-{random_num}.csv")
+
+    for root, dirs, files in os.walk(destination):
+        for file in files:
+            if file.endswith(".jl"):
+                file_path: Path = Path(root, file)
+                start_extraction(repo_name=repo_name, file_path=file_path, output_file=csv_output_file)
 
     shutil.rmtree(destination)
 
 
-if __name__ == '__main__':
-    test: str = "https://github.com/anirban166/rsa-cryptosystem"
-    dest: Path = Path("../tmp")
-    dest.mkdir(exist_ok=True)
+def start_script(csv_input: Path):
+    df: DataFrame = read_csv(csv_input)
+    all_repos_names: list[str] = df["name"].to_list()
+    multi_thread_analysis(repo_names=all_repos_names)
 
-    analyze_repo(test, dest)
+
+if __name__ == '__main__':
+    csv_path: Path = Path("./tmp_input_csv.csv")
+
+
+
 
 
 
--- scripts/julia_function_extractor.py
+++ scripts/julia_function_extractor.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from tree_sitter import Parser, Language
 import tree_sitter_julia
 import sys
@@ -10,30 +12,28 @@ parser = Parser(JULIA_LANGUAGE)
 
 i = 0
 
+
 def extract_functions(node, code, functions):
     # print(node.type)
-        
+
     if node.type == "function_definition":
         # Extract the full function text
         function_text = code[node.start_byte:node.end_byte].decode("utf8")
-        
+
         docstring = find_docstring_before_function(code, node.start_byte)
-        
+
         functions.append((docstring, function_text))
 
-        
-        
     # Recursively visit children nodes
     for child in node.children:
         extract_functions(child, code, functions)
 
 
 def find_docstring_before_function(code, start_byte):
-    
     previous_text = function_text = code[0:start_byte].decode("utf8")
-    
-    if previous_text[-4 : -1] == '"""':
-        
+
+    if previous_text[-4: -1] == '"""':
+
         # we split after each triple double quotes to get the last docstring
         previous_text_subsections = previous_text.split('"""')
         return previous_text_subsections[-2]
@@ -41,15 +41,14 @@ def find_docstring_before_function(code, start_byte):
         return None
 
 
-def save_to_csv(repo_name, file_path, output_file, functions):
-
+def save_to_csv(repo_name: str, file_path: Path, output_file: Path, functions):
     # Check if the file exists to set write mode
     write_mode = "a" if os.path.exists(output_file) else "w"
 
     # Write to CSV
     with open(output_file, write_mode, newline='', encoding="utf8") as file:
         writer = csv.writer(file)
-        
+
         # Write header if file is being created
         if write_mode == "w":
             writer.writerow(["repository_name", "file_path", "docstring", "function"])
@@ -59,24 +58,24 @@ def save_to_csv(repo_name, file_path, output_file, functions):
             writer.writerow([repo_name, file_path, function[0], function[1]])
 
 
-
-def main(repo_name, file_path, output_file):
+def start_extraction(repo_name: str, file_path: Path, output_file: Path):
     # Read code from file
     with open(file_path, "r", encoding="utf8") as file:
         code = file.read()
-    
+
     # Parse the code
     tree = parser.parse(bytes(code, "utf8"))
-    
+
     # Extract functions
     functions = []
     extract_functions(tree.root_node, bytes(code, "utf8"), functions)
-    
+
     # Save to CSV
     save_to_csv(repo_name, file_path, output_file, functions)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: python julia_function_extractor.py <repository_name> <path_to_julia_file> <output_file>")
     else:
-        main(sys.argv[1], sys.argv[2], sys.argv[3])
+        start_extraction(sys.argv[1], Path(sys.argv[2]), Path(sys.argv[3]))
